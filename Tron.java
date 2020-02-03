e6ec36f1e5df60668b24de8708f04c8a3a32e2cf
HafinecMiki <60180774+HafinecMiki@users.noreply.github.com>
2020-02-03 11:27:53 +0100

    Create Tron.java

---
+++ Tron.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tron;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Hafi
+ */
+public class Tron {
+
+    public static void main(String[] args) throws SQLException {
+        new Adatok();
+    }
+    
+}
